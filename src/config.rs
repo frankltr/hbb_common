719f6c798d779d1b4a8194fcc8b36de33028d99c
frankltr <146444799+frankltr@users.noreply.github.com>
2025-04-28 17:14:25 +0200

    Update config.rs
    change default server and key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,10 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+//pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+//pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["chandra2.mywire.org"];
+pub const RS_PUB_KEY: &str = "mu4PO1kjCbssz2NmlfuLR77KCFHEfvd17RzHeLFeF30=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
