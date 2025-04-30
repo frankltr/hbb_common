eb3d4e95b90c064e35d1b2c24fdc5ad088cba8fc
frankltr <146444799+frankltr@users.noreply.github.com>
2025-04-30 16:26:47 +0200

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,7 @@ const CHARS: &[char] = &[
 
 //pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 //pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-pub const RENDEZVOUS_SERVERS: &[&str] = &["chandra2.mywire.org"];
-pub const RS_PUB_KEY: &str = "mu4PO1kjCbssz2NmlfuLR77KCFHEfvd17RzHeLFeF30=";
+include!(concat!(env!("OUT_DIR"), "/generated_secrets.rs"));
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
